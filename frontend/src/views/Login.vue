442a05dc297c7563983a4c97585f45cc70a6e864
Pri <161662779+pribeea@users.noreply.github.com>
2026-08-14 07:45:34 -0300

    Adição da logo

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -2,7 +2,7 @@
 <div class="page-login">
     <div class="main-wrapper">
       <div class="sidebar">
-        <div class="logo-container">🐾 PETHOPE</div>
+        <div class="logo-container"><img src="/pata-branca.png" class="logo-pata" alt="" />PETHOPE</div>
         <div class="dog-image-container">
           <img src="/imagens/img_login.png" alt="Pets" class="pet-img" />
         </div>
