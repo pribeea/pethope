62ffd2d06cec9169238d322aedcea0072c97aa59
Pri <161662779+pribeea@users.noreply.github.com>
2026-09-17 11:44:37 -0300

    Ajuste de rotas

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,23 @@
 <template>
+  <Navbar v-if="mostrarNavbar" />
   <router-view />
 </template>
 
 <script setup>
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+import Navbar from './components/Navbar.vue'
+
+const route = useRoute()
+
+const rotasPublicas = [
+  'home',
+  'cadastro',
+  'opcoes_login',
+  'login',
+  'cadastro_ong',
+  'login_ong'
+]
+
+const mostrarNavbar = computed(() => !rotasPublicas.includes(route.name))
 </script>
