a12eca7e9956523180313f788b7ba8783ce07f63
Pri <161662779+pribeea@users.noreply.github.com>
2026-09-17 11:55:10 -0300

    Ajuste de formulário de voluntário

--- frontend/src/views/FormularioVoluntario.vue
+++ frontend/src/views/FormularioVoluntario.vue
@@ -1,11 +1,6 @@
 <template>
   <div class="page-formulario-voluntario">
-    <header>
-      <h2><img src="/pata-branca.png" class="logo-pata" alt="" /> PetHope</h2>
-      <a href="#" @click.prevent="sair">Sair</a>
-    </header>
-
-    <main class="container">
+<main class="container">
 
       <div v-if="carregando" class="mensagem">Carregando atividade...</div>
 
