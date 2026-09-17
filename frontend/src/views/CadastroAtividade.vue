860bd064feee760efbe8ffc480056fefac218b5a
Pri <161662779+pribeea@users.noreply.github.com>
2026-09-17 11:52:31 -0300

    Ajustes

--- frontend/src/views/CadastroAtividade.vue
+++ frontend/src/views/CadastroAtividade.vue
@@ -1,12 +1,6 @@
 <template>
 <div class="page-cadastro-atividade">
-    <header>
-      <h2>
-        <img src="/pata-branca.png" class="logo-pata" alt="" />PetHope</h2>
-      <a href="#" @click.prevent="sair">Sair</a>
-    </header>
-
-    <main class="container">
+<main class="container">
       <h1>Cadastro de atividade</h1>
       <p>Cadastre uma nova oportunidade de voluntariado.</p>
 
