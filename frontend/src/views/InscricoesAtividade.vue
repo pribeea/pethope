0af4552c709132eaa4d8aef112b28f1bc1c8ff11
Pri <161662779+pribeea@users.noreply.github.com>
2026-09-17 11:55:48 -0300

    Ajustes nas incrições

--- frontend/src/views/InscricoesAtividade.vue
+++ frontend/src/views/InscricoesAtividade.vue
@@ -1,11 +1,6 @@
 <template>
   <div class="page-inscricoes">
-    <header>
-      <h2><img src="/pata-branca.png" class="logo-pata" alt="" /> PetHope</h2>
-      <a href="#" @click.prevent="sair">Sair</a>
-    </header>
-
-    <main class="container">
+<main class="container">
       <h1>Voluntários inscritos</h1>
 
       <p v-if="carregando" class="mensagem">Carregando inscrições...</p>
